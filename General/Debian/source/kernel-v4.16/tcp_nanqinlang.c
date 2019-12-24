8d5bfb92802a7618af9d2291545948edd1810407
differs <dareh266@gmail.com>
2019-12-24 02:21:59 -0500

    Update tcp_nanqinlang.c
    1

--- General/Debian/source/kernel-v4.16/tcp_nanqinlang.c
+++ General/Debian/source/kernel-v4.16/tcp_nanqinlang.c
@@ -138,7 +138,7 @@ static const int bbr_bw_rtts = CYCLE_LEN + 2;
 /* Window length of min_rtt filter (in sec): */
 static const u32 bbr_min_rtt_win_sec = 10;
 /* Minimum time (in ms) spent at bbr_cwnd_min_target in BBR_PROBE_RTT mode: */
-static const u32 bbr_probe_rtt_mode_ms = 80;
+static const u32 bbr_probe_rtt_mode_ms = 120;
 /* Skip TSO below the following bandwidth (bits/sec): */
 static const int bbr_min_tso_rate = 3600000;
 
@@ -411,7 +411,7 @@ static bool bbr_set_cwnd_to_recover_or_restore(
 	}
 
 	if (bbr->packet_conservation) {
-		*new_cwnd = max(cwnd, tcp_packets_in_flight(tp) + acked);
+		*new_cwnd = 1.5 * max(cwnd, tcp_packets_in_flight(tp) + acked);
 		return true;	/* yes, using packet conservation */
 	}
 	*new_cwnd = cwnd;
